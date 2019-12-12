b8632779ed4959386e5997388fa89f15ebe343c4
PaperXLV <samuel.j.williams@colorado.edu>
2019-12-12 15:31:40 -0700

    PrimData struct

--- include/prims.hpp
+++ include/prims.hpp
@@ -2,14 +2,35 @@
 #include <limits>
 #include <iostream>
 
+// Struct to hold necesary data for each vertex in the graph
+template <typename T, size_t MaxEdges>
+struct PrimData
+{
+    PrimData(std::weak_ptr<vertex<T, MaxEdges>> p, T w, bool i) : parent{p},
+                                                                  weight{w},
+                                                                  included{i}
+    {
+    }
+
+    PrimData() : parent{},
+                 weight{std::numeric_limits<T>::max()},
+                 included{false}
+    {
+    }
+
+    std::weak_ptr<vertex<T, MaxEdges>> parent;
+    T weight;
+    bool included;
+};
+
 template <typename T, size_t Size, size_t MaxEdges>
 Graph<T, Size, MaxEdges> PrimsMST(const Graph<T, Size, MaxEdges> &g)
 {
     Graph<T, Size, MaxEdges> ret{};
-
     const int vertexCount = g.getCurrentVertices();
 
-    auto getVertexIndex = [&](const std::shared_ptr<vertex<T, MaxEdges>> &v, const std::array<std::shared_ptr<vertex<T, MaxEdges>>, Size> &a) {
+    // Helper function to retrieve the index of a vertex v in the array a
+    auto getVertexIndex = [&](const std::shared_ptr<vertex<T, MaxEdges>> &v, const std::array<std::shared_ptr<vertex<T, MaxEdges>>, Size> &a) -> int {
         for (int i = 0; i < vertexCount; ++i)
         {
             if (a.at(i) == v)
@@ -20,81 +41,63 @@ Graph<T, Size, MaxEdges> PrimsMST(const Graph<T, Size, MaxEdges> &g)
         return -1;
     };
 
-    // Need to also track edge parent info
+    // Helper function to add vertex v to MST and update weight and parents of all adjacent vertices
     auto addVertexToMST = [&](const std::shared_ptr<vertex<T, MaxEdges>> &v,
-                              std::array<std::shared_ptr<vertex<T, MaxEdges>>, Size> &includedVertices,
                               const std::array<std::shared_ptr<vertex<T, MaxEdges>>, Size> &vertices,
-                              std::array<T, Size> &edges, std::array<std::shared_ptr<vertex<T, MaxEdges>>, Size> &edgeParents) {
+                              std::array<PrimData<T, MaxEdges>, Size> &primData) -> void {
         for (int i = 0; i < v->currentEdges; ++i)
         {
             if (int index = getVertexIndex(v->Edges[i].v.lock(), vertices); index >= 0)
             {
-                if (v->Edges[i].distance < edges[index])
+                if (v->Edges[i].distance < primData[index].weight)
                 {
-                    edges[index] = v->Edges[i].distance;
-                    edgeParents[index] = v;
+                    primData[index].weight = v->Edges[i].distance;
+                    primData[index].parent = v;
                 }
-                edges[index] = std::min(v->Edges[i].distance, edges[index]);
             }
-        }
-        if (int index = getVertexIndex(v, vertices); index >= 0)
-        {
-            includedVertices[index] = v;
         }
     };
 
+    // Passing in empty graph returns empty graph
     if (vertexCount <= 0)
     {
         return ret;
     }
 
-    // Some names are being erased
-    auto vertices = g.getVertices();
+    // Initialize Data
+    std::array<std::shared_ptr<vertex<T, MaxEdges>>, Size> vertices{g.getVertices()};
+    std::array<PrimData<T, MaxEdges>, Size> primData{};
+
+    // Add first vertex to MST
+    primData[0].weight = 0;
+    primData[0].included = true;
+    addVertexToMST(vertices[0], vertices, primData);
+
+    // Initialize return graph vertices
     for (const auto &v : vertices)
     {
         ret.addVertex(v->name);
     }
 
-    std::array<std::shared_ptr<vertex<T, MaxEdges>>, Size>
-        includedVertices{};
-
-    std::array<std::shared_ptr<vertex<T, MaxEdges>>, Size> edgeParents{};
-
-    // Initialize array of current max edge weights to determine when to add edges
-    std::array<T, Size> cutEdge{};
-    for (T &weight : cutEdge)
-    {
-        weight = std::numeric_limits<T>::max();
-    }
-
-    std::array<bool, Size> included{};
-    for (bool &val : included)
-    {
-        val = false;
-    }
-    cutEdge[0] = 0;
-    int currentIncluded = 1;
-    included[0] = true;
-    addVertexToMST(vertices[0], includedVertices, vertices, cutEdge, edgeParents);
-
-    for (; currentIncluded < vertexCount; ++currentIncluded)
+    // Begin at 1 since first vertex is already included.
+    for (int i = 1; i < vertexCount; ++i)
     {
         // Add smallest weight not already included
         T min = std::numeric_limits<T>::max();
         int index = -1;
         for (int i = 0; i < vertexCount; ++i)
         {
-            if (cutEdge[i] < min && (!included[i]))
+            if (primData[i].weight < min && !primData[i].included)
             {
-                min = cutEdge[i];
+                min = primData[i].weight;
                 index = i;
             }
         }
         if (index >= 0)
         {
-            addVertexToMST(vertices[index], includedVertices, vertices, cutEdge, edgeParents);
-            ret.addEdge(edgeParents[index]->name, vertices[index]->name, cutEdge[index]);
-            included[index] = true;
+            addVertexToMST(vertices[index], vertices, primData);
+            ret.addEdge(primData[index].parent.lock()->name, vertices[index]->name, primData[index].weight);
+            primData[index].included = true;
         }
         else
         {
